5ad257e0b3e95dadd1fe67bd5d992aced9f6f7c4
pop-ideasKO <koustavd60@gmail.com>
2026-05-15 14:22:31 +0530

    Welcome email implemented

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId,res) => {
-    const { JWT_SECRET } = process.env;
+    const { JWT_SECRET } = ENV;
     if (!JWT_SECRET) {
         throw new Error("JWT_SECRET is not defined in environment variables");
     }
@@ -12,7 +13,7 @@ export const generateToken = (userId,res) => {
         maxAge: 7*24*60*60*1000, // 7 days in milliseconds
         httpOnly: true, //prevent client-side JavaScript from accessing the cookie
         sameSite: "strict", // restricts the cookie to be sent only in a first-party context
-        secure: process.env.NODE_ENV === "production" ? true:false, // ensures the cookie is sent over HTTPS in production
+        secure: ENV.NODE_ENV === "production" ? true:false, // ensures the cookie is sent over HTTPS in production
     });
 
     return token;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import {ENV} from "./lib/env.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -10,7 +11,7 @@ import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
-const PORT= process.env.PORT || 3000;
+const PORT= ENV.PORT || 3000;
 
 const app=express();
 app.use(express.json());
@@ -22,7 +23,7 @@ app.use("/api/messages",messageRoutes);
 
 
 // make ready for deployment
-if(process.env.NODE_ENV === "production"){
+if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
     app.get("*",(req,res) => {
